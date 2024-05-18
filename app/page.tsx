abd3b83bfc7c3b951771e1aec141c01a8d362b40
Tobias <Windows@Tobias>
2024-05-18 13:15:11 -0300

    boton nav bar para moviles hecho, comienzo de vinos destacados

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,27 @@
 import Image from 'next/image';
 import bgImage from '../public/background.jpg';
-import NavBar from './ui/components/navBar';
-import { maven_Pro } from './fonts';
-import logo from '../public/logo.png';
+import NavBar from '../app/ui/components/navBar';
+import { maven_Pro } from '../app/fonts';
 
 const Bodine = () => {
   return (
     <>
-      <div className="relative w-full min-h-96 h-screen max-h-max" style={{maxHeight: '800px'}}>
+      <div className="relative w-full min-h-96 h-screen max-h-max" style={{ maxHeight: '800px' }}>
         <Image src={bgImage} alt="Imagen de fondo" layout="fill" objectFit="cover" objectPosition='30% center' quality={100} />
         <div className="absolute inset-0 bg-black bg-opacity-35" />
         
-        <div className={`${maven_Pro.className} absolute inset-0 flex items-center justify-center`} >
+        <div className={`${maven_Pro.className} absolute inset-1 mt-20 flex items-center justify-center`}>
           <h1 className="text-white text-center sm:text-2xl lg:text-3xl text-lg">A UN PASO DE DESCUBRIR LOS <strong>MEJORES VINOS</strong></h1>
         </div>
       </div>
       <div className="absolute top-0 left-0 w-full">
-          <NavBar />
+        <NavBar />
       </div>
-      <div>
-      <Image
-            src={logo}
-            alt="Logo"
-            className="sm:w-40 sm:h-15 lg:w-60 lg:h-20 transition-all duration-500 ease-in-out transform hover:scale-110"
-          />
-      </div>
-      <div className="border-b border-black sm:min-w-[300px] sm:max-w-[600px] sm:mx-auto "></div>
+      <div className="border-b border-black sm:min-w-[300px] sm:max-w-[600px] sm:mx-auto"></div>
       <div>
         <h2 className={`${maven_Pro.className} font-semibold text-4xl text-center my-5`}>Vinos Destacados</h2>
       </div>
-      <div className="border-b border-black sm:min-w-[300px] sm:max-w-[600px] sm:mx-auto "></div>
+      <div className="border-b border-black sm:min-w-[300px] sm:max-w-[600px] sm:mx-auto"></div>
       <div className='h-40 text-center py-20'>Componente slider</div>
     </>
   );
--- app/ui/components/navBar.tsx
+++ app/ui/components/navBar.tsx
@@ -1,12 +1,37 @@
+"use client";
+import './styles.css';
+import { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import logo from '../../../public/logo.png';
 import Link from 'next/link';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      menuRef.current.style.maxHeight = `${menuRef.current.scrollHeight}px`;
+    } else {
+      menuRef.current.style.maxHeight = '0px';
+    }
+  }, [isMenuOpen]);
+
   return (
     <>
-      <nav className="flex items-center justify-end flex-wrap bg-transparent z-50 p-6">
-        <div className="hidden w-full flex-grow sm:flex lg:items-center lg:w-auto text-lg md:text-xl lg:flex-grow justify-between place-content-center">
+      <nav className="flex flex-col sm:flex-row items-center justify-between flex-wrap bg-transparent z-50 p-3 mx-7">
+        <div className="sm:hidden w-full flex justify-start items-center mb-4">
+          <Image
+            src={logo}
+            alt="Logo"
+            className="w-40 h-15 transition-all duration-500 ease-in-out transform hover:scale-110"
+          />
+        </div>
+        <div className={`hidden w-full flex-grow sm:flex lg:items-center lg:w-auto text-lg md:text-xl lg:flex-grow justify-between place-content-center sm:block transition-all duration-500 ease-in-out`}>
           <Link href="/compras" className="block lg:inline-block lg:mt-0 hover:text-2xl transform duration-500 text-white">
             Compras
           </Link>
@@ -14,11 +39,13 @@ function NavBar() {
           <Link href="/nosotros" className="block lg:inline-block lg:mt-0 text-white hover:text-2xl transform duration-500">
             Nosotros
           </Link>
-          <Image
-            src={logo}
-            alt="Logo"
-            className="sm:w-40 sm:h-15 lg:w-60 lg:h-20 transition-all duration-500 ease-in-out transform hover:scale-110"
-          />
+          <div className="hidden sm:block">
+            <Image
+              src={logo}
+              alt="Logo"
+              className="sm:w-40 sm:h-15 lg:w-60 lg:h-20 transition-all duration-500 ease-in-out transform hover:scale-110"
+            />
+          </div>
           <Link href="/suscripciones" className="block lg:inline-block lg:mt-0 text-white hover:text-2xl transform duration-500">
             Suscripciones
           </Link>
@@ -26,22 +53,28 @@ function NavBar() {
             Log In
           </Link>
         </div>
-        <div className='sm:hidden w-10 h-10 pr-0 pt-0 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
-          <button data-collapse-toggle="navbar-cta" type="button" aria-controls="navbar-cta" aria-expanded="false">
-            <svg className="w-10 h-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+        <div className="absolute right-0 top-3 h-16 w-16 sm:hidden pr-0 pt-0 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-2">
+          <button onClick={toggleMenu} aria-controls="navbar-cta" aria-expanded={isMenuOpen}>
+            <svg className={`w-10 h-10 transition-transform duration-500 ${isMenuOpen ? 'rotate-45' : ''}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <path
-                stroke="currentColor"
+                stroke="white"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
+                d={isMenuOpen ? "M6 6L18 18M6 18L18 6" : "M4 6h16M4 18h16"}
               />
             </svg>
           </button>
         </div>
       </nav>
+      <div ref={menuRef} className={`sm:hidden  w-full overflow-hidden transition-max-height duration-500 ease-in-out`}>
+        <Link href="/compras" className="block py-2 text-center text-white">Compras</Link>
+        <Link href="/nosotros" className="block py-2 text-center text-white">Nosotros</Link>
+        <Link href="/suscripciones" className="block py-2 text-center text-white">Suscripciones</Link>
+        <Link href="/login" className="block py-2 text-center text-white">Log In</Link>
+      </div>
     </>
   );
-};
+}
 
 export default NavBar;
