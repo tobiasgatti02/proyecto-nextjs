fe2875bcf79d45de2b08fa46065a1597772456e3
Guido Reale <realeguido1@gmail.com>
2024-05-31 14:31:00 -0300

    nombres accesibles en navbar

--- app/ui/components/navBar.tsx
+++ app/ui/components/navBar.tsx
@@ -120,7 +120,7 @@ function NavBar({ bgColorTop, bgColorScrolled,text,logo,logoWidth, logoHeight }:
                 />
               </Link>
               <div className="pt-1 z-50">
-                <button onClick={toggleMenu} aria-controls="navbar-cta" aria-expanded={isMenuOpen}>
+                <button onClick={toggleMenu} aria-controls="navbar-cta" aria-label='desplegar la barra de navegación' aria-expanded={isMenuOpen}>
                   <svg className={`w-10 h-12 mx-3 transition-transform duration-500 ${isMenuOpen ? 'rotate-45' : ''}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <path
                       stroke="white"
@@ -138,7 +138,7 @@ function NavBar({ bgColorTop, bgColorScrolled,text,logo,logoWidth, logoHeight }:
                   <Link href="/nosotros" className="block py-2 text-center text-white ">Nosotros</Link>
                   <Link href="/suscripciones" className="block py-2 text-center text-white ">Suscripciones</Link>
                   <Link href="/login" className="block py-2 text-center text-white ">Log In</Link>
-                  <Link href="/carrito" className="block py-2 lg:mt-0">
+                  <Link href="/carrito" className="block py-2 lg:mt-0" aria-label='ir al carrito'>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px"className='mx-auto' viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></Link>
                 </div>
               </div>
