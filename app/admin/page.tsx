7999f30e498b2b233605bc7494fb0d0ea8608992
Tobias <Windows@Tobias>
2024-06-04 11:15:08 -0300

    deletion edition admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,11 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { insertVino } from '../lib/actions';
-import Router from 'next/router';
-  import { getSession } from 'next-auth/react';
+import React, { useState } from 'react';
+import { insertVino, deleteVino, updateVino } from '../lib/actions';
 
-const AddWineForm = () => {
-
-  
+const WineForm = () => {
   const [wineData, setWineData] = useState({
     wine: '',
-    type: '',
+    wine_category: '',
     price: 0,
     image: '',
     winery: '',
@@ -18,23 +14,37 @@ const AddWineForm = () => {
     location: ''
   });
 
-  
+  const [wineIdToDelete, setWineIdToDelete] = useState('');
+  const [wineIdToEdit, setWineIdToEdit] = useState('');
+  const [editWineData, setEditWineData] = useState({
+    wine: '',
+    wine_category: '',
+    price: 0,
+    image: '',
+    winery: '',
+    average_rating: 0,
+    reviews: '',
+    location: ''
+  });
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setWineData({ ...wineData, [name]: value });
   };
 
+  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setEditWineData({ ...editWineData, [name]: value });
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // Envía los datos del vino al backend para procesar la inserción
       await insertVino(wineData);
       console.log('Wine added successfully!');
-
-      // Limpia el formulario después de la inserción exitosa
       setWineData({
         wine: '',
-        type: '',
+        wine_category: '',
         price: 0,
         image: '',
         winery: '',
@@ -47,20 +57,76 @@ const AddWineForm = () => {
     }
   };
 
+  const handleDelete = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await deleteVino(Number(wineIdToDelete));
+      console.log('Wine deleted successfully!');
+      setWineIdToDelete('');
+    } catch (error) {
+      console.error('Error deleting wine:', error);
+    }
+  };
+
+  const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await updateVino(Number(wineIdToEdit), editWineData);
+      console.log('Wine edited successfully!');
+      setWineIdToEdit('');
+      setEditWineData({
+        wine: '',
+        wine_category: '',
+        price: 0,
+        image: '',
+        winery: '',
+        average_rating: 0,
+        reviews: '',
+        location: ''
+      });
+    } catch (error) {
+      console.error('Error editing wine:', error);
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="wine" value={wineData.wine} onChange={handleChange} placeholder="Wine Name" />
-      <input type="text" name="type" value={wineData.type} onChange={handleChange} placeholder="Wine Type" />
-      <input type="number" name="price" value={wineData.price} onChange={handleChange} placeholder="Price" />
-      <input type="text" name="image" value={wineData.image} onChange={handleChange} placeholder="Image URL" />
-      <input type="text" name="winery" value={wineData.winery} onChange={handleChange} placeholder="Winery" />
-      <input type="number" name="average_rating" value={wineData.average_rating} onChange={handleChange} placeholder="Average Rating" />
-      <input type="text" name="reviews" value={wineData.reviews} onChange={handleChange} placeholder="Reviews" />
-      <input type="text" name="location" value={wineData.location} onChange={handleChange} placeholder="Location" />
-      <br />
-      <button type="submit">Add Wine</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h3>Add Wine</h3>
+        <input type="text" name="wine" value={wineData.wine} onChange={handleChange} placeholder="Wine Name" />
+        <input type="text" name="wine_category" value={wineData.wine_category} onChange={handleChange} placeholder="Wine Type" />
+        <input type="number" name="price" value={wineData.price} onChange={handleChange} placeholder="Price" />
+        <input type="text" name="image" value={wineData.image} onChange={handleChange} placeholder="Image URL" />
+        <input type="text" name="winery" value={wineData.winery} onChange={handleChange} placeholder="Winery" />
+        <input type="number" name="average_rating" value={wineData.average_rating} onChange={handleChange} placeholder="Average Rating" />
+        <input type="text" name="reviews" value={wineData.reviews} onChange={handleChange} placeholder="Reviews" />
+        <input type="text" name="location" value={wineData.location} onChange={handleChange} placeholder="Location" />
+        <br />
+        <button type="submit">Add Wine</button>
+      </form>
+
+      <form onSubmit={handleDelete}>
+        <h3>Delete Wine</h3>
+        <input type="number" value={wineIdToDelete} onChange={(e) => setWineIdToDelete(e.target.value)} placeholder="Wine ID" />
+        <button type="submit">Delete Wine</button>
+      </form>
+
+      <form onSubmit={handleEdit}>
+        <h3>Edit Wine</h3>
+        <input type="number" value={wineIdToEdit} onChange={(e) => setWineIdToEdit(e.target.value)} placeholder="Wine ID" />
+        <input type="text" name="wine" value={editWineData.wine} onChange={handleEditChange} placeholder="Wine Name" />
+        <input type="text" name="wine_category" value={editWineData.wine_category} onChange={handleEditChange} placeholder="Wine Type" />
+        <input type="number" name="price" value={editWineData.price} onChange={handleEditChange} placeholder="Price" />
+        <input type="text" name="image" value={editWineData.image} onChange={handleEditChange} placeholder="Image URL" />
+        <input type="text" name="winery" value={editWineData.winery} onChange={handleEditChange} placeholder="Winery" />
+        <input type="number" name="average_rating" value={editWineData.average_rating} onChange={handleEditChange} placeholder="Average Rating" />
+        <input type="text" name="reviews" value={editWineData.reviews} onChange={handleEditChange} placeholder="Reviews" />
+        <input type="text" name="location" value={editWineData.location} onChange={handleEditChange} placeholder="Location" />
+        <br />
+        <button type="submit">Edit Wine</button>
+      </form>
+    </div>
   );
 };
 
-export default AddWineForm;
+export default WineForm;
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,21 +1,27 @@
 'use server'
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
 import { db } from '@vercel/postgres';
 
-
-
+export async function deleteVino(wineId: number) {
+  try {
+    const query = `
+      DELETE FROM vinos
+      WHERE id = $1
+    `;
+    await db.query(query, [wineId]);
+  } catch (error: any) {
+    throw new Error('Error deleting wine: ' + error.message);
+  }
+}
 
 export async function insertVino(wineData: any) {
   try {
-    // Inserta los datos del vino en la tabla de vinos
     const query = `
-      INSERT INTO vinos (wine, type, price, image, winery, average_rating, reviews, location)
+      INSERT INTO vinos (wine, wine_category, price, image, winery, average_rating, reviews, location)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     `;
     const values = [
       wineData.wine,
-      wineData.type,
+      wineData.wine_category,
       wineData.price,
       wineData.image,
       wineData.winery,
@@ -29,23 +35,26 @@ export async function insertVino(wineData: any) {
   }
 }
 
-
-
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
+export async function updateVino(wineId: number, wineData: any) {
   try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
+    const query = `
+      UPDATE vinos
+      SET wine = $1, wine_category = $2, price = $3, image = $4, winery = $5, average_rating = $6, reviews = $7, location = $8
+      WHERE id = $9
+    `;
+    const values = [
+      wineData.wine,
+      wineData.wine_category,
+      wineData.price,
+      wineData.image,
+      wineData.winery,
+      wineData.average_rating,
+      wineData.reviews,
+      wineData.location,
+      wineId,
+    ];
+    await db.query(query, values);
+  } catch (error: any) {
+    throw new Error('Error updating wine: ' + error.message);
   }
 }
