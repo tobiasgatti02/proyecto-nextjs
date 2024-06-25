8bb12bd775319e14635b0cef142f962ddb97a621
Tobias <Windows@Tobias>
2024-06-25 18:30:58 -0300

    navBar test

--- app/ui/components/navBar.tsx
+++ app/ui/components/navBar.tsx
@@ -152,13 +152,7 @@ function NavBar({ bgColorTop, bgColorScrolled,text,logo,logoWidth, logoHeight }:
                   <Link href="/compras" className="block py-2 text-center text-white ">Shop</Link>
                   <Link href="/maridaje" className="block py-2 text-center text-white ">Food Pairing</Link>
                   <Link href="/suscripciones" className="block py-2 text-center text-white ">Suscriptions</Link>
-                  {session ? (
-                  <button onClick={() => signOut()} className="block py-2 text-center text-white w-full">
-                    Log Out
-                  </button>
-                ) : (
-                  <Link href="/auth/login" className="block py-2 text-center text-white ">Log In</Link>
-                )}                  <Link href="/carrito" className="block py-2 lg:mt-0" aria-label='ir al carrito'>
+                  <Link href="/carrito" className="block py-2 lg:mt-0" aria-label='ir al carrito'>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px"className='mx-auto' viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></Link>
                 </div>
               </div>
